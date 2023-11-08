7d7827391d798180e5e612a84ed035a1dfc03cd0
Swarnajit Adhikary <Swarnajit.Adhikary@hyland.com>
2023-11-08 13:39:52 +0530

    Committed on 8th Nov

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
 
-function App() {
+// 0. Import depdendencies
+import * as tf from "@tensorflow/tfjs"
+import * as speech from "@tensorflow-models/speech-commands"
+
+
+const App = () => {
+// 1. Create model and action states
+const [model, setModel] = useState(null)
+const [action, setAction] = useState(null)
+const [labels, setLabels] = useState(null) 
+
+// 2. Create Recognizer
+const loadModel = async () =>{
+  const recognizer = await speech.create("BROWSER_FFT")
+  console.log('Model Loaded')
+  await recognizer.ensureModelLoaded();
+  console.log(recognizer.wordLabels())
+  setModel(recognizer)
+  setLabels(recognizer.wordLabels())
+}
+
+useEffect(()=>{loadModel()}, []); 
+
+// 
+function argMax(arr){
+  return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+// 3. Listen for Actions
+const recognizeCommands = async () =>{
+  console.log('Listening for commands')
+  model.listen(result=>{
+
+    var abcd = labels[argMax(Object.values(result.scores))]
+    console.log(abcd)
+
+    if(abcd == "stop")
+      window.close();
+
+      if(abcd == "up")
+        scrollUp();
+
+      if(abcd == "go")
+        scrollDown();
+
+    //console.log(result.spectrogram)
+    setAction(labels[argMax(Object.values(result.scores))])
+  }, {includeSpectrogram:true, probabilityThreshold:0.8})
+  //setTimeout(()=>model.stopListening(), 10e3)
+}
+
+function scrollDown() {
+  window.setTimeout(() => {
+  }, 5000);
+  window.scrollBy(0, 75);
+  console.log("down");
+  //alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+}
+
+function scrollUp() {
+  window.scrollBy(0, -75);
+  //alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  console.log("up");
+  window.setTimeout(() => {
+  }, 5000);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Button variant="outline-primary" onClick={recognizeCommands}>Command</Button>
+          {action ? <div id="action">{action}</div>:<div>No Action Detected</div> }
     </div>
   );
 }
